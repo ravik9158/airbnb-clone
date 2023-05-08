928a2f5e570aee0a4e095975485e8c3f3bfb463e
ravik9158 <ravik9158@hotmail.com>
2023-05-09 04:03:27 +0530

    finishing login, adding user to session & logout functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-import {Nunito} from "next/font/google";
+import { Nunito } from "next/font/google";
 import './globals.css'
 import Navbar from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./components/providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
   title: 'Airbnb',
@@ -13,17 +15,19 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
-        <ToasterProvider/>
+        <ToasterProvider />
         <RegisterModal />
-        <Navbar />
+        <LoginModal />
+        <Navbar currentUser={currentUser}/>
         {children}
       </body>
     </html>
